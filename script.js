ab1818e10bbfc0f11728cfd15065d800889bf19c
Igor <igoor_silva13@live.com>
2023-09-09 18:16:30 -0300

    Refactoring in code JS

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   //substituir a imagem
-  if(html.classList.contains("light")) {
+  if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/assets/Avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/assets/Avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
-  
 }
